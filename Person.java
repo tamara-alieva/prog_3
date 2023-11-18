df8c9fd527dba6dc8296499864156d570786b2b8
Tamara <tamaraavgust14@gmail.com>
2023-11-18 23:02:54 +0700

    Добавление методов

--- Person.java
+++ Person.java
@@ -23,4 +23,46 @@ public class Person {
         this.name = name;
         this.balance = balance;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getBalance() {
+        return this.balance;
+    }
+
+    public void input() {
+        Scanner scanner = new Scanner(System.in);
+        String temp;
+        System.out.println("** Ввод данных человека **");
+        do {
+            System.out.print("Введите имя: ");
+            this.name = scanner.next();
+        } while (this.name.isEmpty() || !Checking.nameCheck(this.name));
+        do {
+            System.out.print("Введите баланс: ");
+            temp = scanner.next();
+        } while (temp.isEmpty() || !Checking.intCheck(temp));
+        this.balance = Integer.parseInt(temp);
+        System.out.println("Данные успешно введены!\n");
+    }
+
+    public void output() {
+        if (this.name.isEmpty()) {
+            System.out.println("Данные о человеке отсутствуют!");
+        } else {
+            System.out.println("Данные о человеке:");
+            System.out.println("-Имя: " + this.name);
+            System.out.println("-Баланс: " + this.balance);
+        }
+    }
 }
