f860e6d178342d2628c53e6e678bffc7f5ae24f1
Tamara <tamaraavgust14@gmail.com>
2023-11-18 23:39:31 +0700

    Добавление методов

--- Passenger.java
+++ Passenger.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Passenger extends Person {
     private boolean payment_method;
 
@@ -19,5 +21,45 @@ public class Passenger extends Person {
     public Passenger(String name, int balance, boolean method) {
         super(name, balance);
         this.payment_method = method;
+    }
+
+    public void setMethod(boolean method) {
+        this.payment_method = method;
+    }
+
+    public boolean getMethod() {
+        return this.payment_method;
+    }
+
+    @Override
+    public void input() {
+        Scanner scanner = new Scanner(System.in);
+        String temp;
+        System.out.println("** Ввод данных о пассажире: " + this.getName() + " **");
+        do {
+            System.out.print("Введите способ оплаты (0 - Наличные, 1 - Банковская карта): ");
+            temp = scanner.next();
+        } while (!temp.equals("0") && !temp.equals("1"));
+        this.payment_method = Boolean.parseBoolean(temp);
+        System.out.println("Данные успешно введены!\n");
+        scanner.close();
+    }
+
+    @Override
+    public void output() {
+        System.out.println("Данные о пассажире:");
+        System.out.println("-Имя: " + this.getName());
+        System.out.println("-Баланс: " + this.getBalance());
+        System.out.print("-Способ оплаты: ");
+        if (this.getMethod()) {
+            System.out.println("Банковская карта\n");
+        } else {
+            System.out.println("Наличные\n");
+        }
+    }
+
+    public void takePayment(int payment) {
+        int old_balance = this.getBalance();
+        this.setBalance(old_balance - payment);
     }
 }
