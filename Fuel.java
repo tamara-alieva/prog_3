6d7caef81ea2e96e1725d31fae8f38a3ab9a3c82
Tamara <tamaraavgust14@gmail.com>
2023-11-18 23:49:48 +0700

    Конструкторы

---
+++ Fuel.java
@@ -0,0 +1,11 @@
+public class Fuel {
+    private boolean capacity;
+
+    public Fuel() {
+        this.capacity = false;
+    }
+
+    public Fuel(boolean capacity) {
+        this.capacity = capacity;
+    }
+}
