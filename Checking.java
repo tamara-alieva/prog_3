f3ab652fe692153d78602ef98d89fbdecc8529e6
Tamara <tamaraavgust14@gmail.com>
2023-11-18 22:56:08 +0700

    Статический метод intCheck

--- Checking.java
+++ Checking.java
@@ -11,4 +11,17 @@ public class Checking {
         }
         return f;
     }
+
+    static private boolean intCheck(String balance) {
+        boolean f = true;
+        for (int i = 0; i < balance.length(); i++) {
+            if (balance.charAt(i) >= '0' && balance.charAt(i) <= '9') {
+                f = true;
+            } else {
+                f = false;
+                break;
+            }
+        }
+        return f;
+    }
 }
